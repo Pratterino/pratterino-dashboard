79ac7ef3d5b312209825e3d9735dcb26d5a303a6
Pär Strandberg <par.strandberg@comhem.com>
2017-11-09 18:32:14 +0100

    Omnipollo gets their updated date.

--- jobs/omnipollo.rb
+++ jobs/omnipollo.rb
@@ -5,10 +5,21 @@ def get_omnipollo_beercam_url
   html = open('http://www.omnipolloshatt.com/')
   doc = Nokogiri::HTML(html)
 
-  img = doc.xpath("//div[@class='front-block'][1]/img").select {|img| img['src'].include? 'beercam'}
-  img.first['src'].to_s
+  doc.xpath("//div[@class='front-block']").each {|div|
+    img = div.xpath('./img/@src').to_s
+
+    if img.include? 'beercam'
+      return {
+          :url => img,
+          :updated => div.xpath('./p/text()').to_s
+      }
+    end
+  }
 end
 
 SCHEDULER.every '10m', :first_in => 0 do
-  send_event('omnipollo', {:url => get_omnipollo_beercam_url})
+  omni = get_omnipollo_beercam_url
+  unless omni.nil?
+    send_event('omnipollo', {:url => omni[:url], :updated => omni[:updated]})
+  end
 end
